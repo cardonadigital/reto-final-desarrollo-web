2d07c1feb6e72c87e9aa4d7cbce00d2eefa4ca0f
dcardonamoreno <danielcardona3d@gmail.com>
2022-07-28 20:29:24 -0500

    Revert "implements method create task(when task is created also creates log"
    This reverts commit 5efa01f6a19a9464e081932e8ea550590b244c66.

--- src/main/java/org/sofka/mykrello/model/repository/ColumnForBoardRepository.java
+++
@@ -1,6 +0,0 @@
-package org.sofka.mykrello.model.repository;
-
-import org.sofka.mykrello.model.domain.ColumnForBoardDomain;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ColumnForBoardRepository extends JpaRepository<ColumnForBoardDomain, Integer> { }
--- src/main/java/org/sofka/mykrello/model/repository/LogRepository.java
+++
@@ -1,6 +0,0 @@
-package org.sofka.mykrello.model.repository;
-
-import org.sofka.mykrello.model.domain.LogDomain;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LogRepository extends JpaRepository<LogDomain, Integer>{ }
