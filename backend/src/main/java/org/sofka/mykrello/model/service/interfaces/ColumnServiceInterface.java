5844eb8d2f77cbe589fad91dd17735fe48a6fb77
dcardonamoreno <danielcardona3d@gmail.com>
2022-07-28 20:48:31 -0500

    fixing errors (ColumnServiceInterface)

--- backend/src/main/java/org/sofka/mykrello/model/service/interfaces/ColumnServiceInterface.java
+++ backend/src/main/java/org/sofka/mykrello/model/service/interfaces/ColumnServiceInterface.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.sofka.mykrello.model.service.interfaces;
 
 import org.sofka.mykrello.model.domain.ColumnDomain;
@@ -17,23 +16,3 @@ public interface ColumnServiceInterface {
 
     public ColumnDomain delete(Integer id);
 }
-=======
-package org.sofka.mykrello.model.service.interfaces;
-
-import org.sofka.mykrello.model.domain.ColumnDomain;
-
-import java.util.List;
-
-public interface ColumnServiceInterface {
-
-    public List<ColumnDomain> getAll();
-
-    public ColumnDomain findById(Integer id);
-
-    public ColumnDomain create(Integer id, ColumnDomain column);
-
-    public ColumnDomain update(Integer id, ColumnDomain column);
-
-    public ColumnDomain delete(Integer id);
-}
->>>>>>> 6e1b31969ffd0905d646daeffe851573779e6383
